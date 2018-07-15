6056eff0b4c79ef4eaae2803dd5ae136301b8528
MayuriR29 <mayuri.gr@gmail.com>
2018-07-15 22:58:03 +0800

    added authentication

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
-
 const mongoose = require("mongoose");
+const { passport } = require("./config/passport");
+
 const mongodb_uri =
   process.env.MONGODB_URI || "mongodb://localhost/mongoDB-books";
 
 const index = require("./routes/index");
 const books = require("./routes/books");
 const authors = require("./routes/authors");
+const secretsRouter = require("./routes/secretsRouter");
 
 mongoose.connect(mongodb_uri);
 const db = mongoose.connection;
 db.on("error", () => {
-  console.error("An error has occured");
+  console.error("An error has occured====>");
 });
-const app = express();
 
+const app = express();
+app.use(passport.initialize());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,5 +27,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", index);
 books(app);
 authors(app);
+app.use(
+  "/secret",
+  passport.authenticate("jwt", { session: false }),
+  secretsRouter
+);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require("../models/user");
+const { jwtOptions } = require("../config/passport");
+const jwt = require("jsonwebtoken");
 /* GET home page. */
 router.get("/", (req, res, next) => {
   res.json({ message: "hello express-blog-api" });
+});
+//signup
+router.post("/signup", async (req, res, next) => {
+  console.log("in signup", req.body);
+  const { username, password } = req.body;
+  const user = new User({ username, bio: "some bio" });
+  user.setPassword(password);
+  try {
+    await user.save();
+    res.json(user);
+  } catch (err) {
+    console.err("error in /signup", err);
+    next(err);
+  }
+});
+router.get("/users", async (req, res, next) => {
+  const users = await User.find();
+  res.json(users);
+});
+router.post("/signin", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username: username });
+  if (!user) {
+    res.status(401).json({ message: "no such user found" });
+  }
+
+  if (user.validPassword(password)) {
+    const userId = { id: user.id };
+    console.log("user id", userId);
+    const token = jwt.sign(userId, jwtOptions.secretOrKey);
+    res.json({ message: "ok", token: token });
+  } else {
+    res.status(401).json({ message: "passwords did not match" });
+  }
 });
 
 module.exports = router;
